180f417c07903363cf7533bef9f556d90df8ab7c
stefanocoretta <stefano.coretta@gmail.com>
2021-04-18 16:56:57 +0200

    :bug: - use relURL and hardcode url in css

--- assets/js/index.js
+++ assets/js/index.js
@@ -86,7 +86,7 @@ Source:
     {{ range $index, $page := (where .Site.Pages "Section" "docs") -}}
       {
         id: {{ $index }},
-        href: "{{ .Permalink | absURL }}",
+        href: "{{ .Permalink | relURL }}",
         title: {{ .Title | jsonify }},
         description: {{ .Params.description | jsonify }},
         content: {{ .Content | jsonify }}
